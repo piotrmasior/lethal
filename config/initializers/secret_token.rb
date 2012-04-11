53273b446f1b3d2f28fa709aafe9281fe0e6e816
Piotr Mąsior <piotr.masior@polcode.pl>
2012-04-11 21:38:36 +0200

    initial: rails new lethal --database=mysql --skip-test-unit --old-style-hash

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Lethal::Application.config.secret_token = '9d45069972fc94688bd04d5cf7ff04aa5f080879f012599aaa35c7708a566508fdfde54caa184d30fc0fd8c027e03cfd9cfa2b4d119063797aa24f2376ccc899'
