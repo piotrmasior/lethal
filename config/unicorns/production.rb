84af28bf36516911e80e84907604e396995caed6
Piotr Mąsior <pmasior@railsutils.com>
2012-04-15 20:19:21 +0200

    unicorn config

---
+++ config/unicorns/production.rb
@@ -0,0 +1,8 @@
+working_directory "/home/lethal/www/current"
+pid "/home/lethal/www/shared/unicorn.pid"
+stderr_path "/var/log/unicorn_lethal.log"
+stdout_path "/var/log/unicorn_lethal.log"
+
+listen "/tmp/lethal_app.socket"
+worker_processes 1
+timeout 30
