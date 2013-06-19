3e1a8cbc62740df4093ba89a4e0363554a00508a
Piotr Mąsior <pmasior@railsutils.com>
2013-06-19 11:54:40 +0200

    upgrade initializers to rails 4

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -1,7 +1,1 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-Lethal::Application.config.secret_token = '9d45069972fc94688bd04d5cf7ff04aa5f080879f012599aaa35c7708a566508fdfde54caa184d30fc0fd8c027e03cfd9cfa2b4d119063797aa24f2376ccc899'
+Lethal::Application.config.secret_key_base = 'bb115fff1e220df44c19ebbb73b78428dac0968267da12af75bffa47eb17e526dea22507a0e91fb5db1d5ef47b86a3516d98af6561a6ae51a57500fd0845e41c'
